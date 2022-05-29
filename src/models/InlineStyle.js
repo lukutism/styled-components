673ab60ad897671208bf6c2d66b12775265195b3
Lukas Malinauskas <lukutim@gmail.com>
2022-05-29 19:32:56 +0300

    quick fix

--- src/models/InlineStyle.js
+++ src/models/InlineStyle.js
@@ -1,16 +1,16 @@
 // @flow
-import hashStr from 'glamor/lib/hash'
 /* eslint-disable import/no-unresolved */
+import transformDeclPairs from 'css-to-react-native';
+
+import generateComponentId from '../utils/generateComponentId';
+import type { RuleSet } from '../types';
 import StyleSheet from 'react-native-extended-stylesheet'
-import transformDeclPairs from 'css-to-react-native'
+import flatten from '../utils/flatten';
+// $FlowFixMe
+import parse from '../vendor/postcss-safe-parser/parse';
 
-import type { RuleSet } from '../types'
-import flatten from '../utils/flatten'
-import parse from '../vendor/postcss-safe-parser/parse'
-
+let generated = {};
 StyleSheet.build()
-
-const generated = {}
 
 const excludeShorthands = [
   'borderRadius',
@@ -19,47 +19,56 @@ const excludeShorthands = [
   'borderStyle',
 ]
 
+export const resetStyleCache = () => {
+  generated = {};
+};
+
 /*
  InlineStyle takes arbitrary CSS and generates a flat object
  */
-export default class InlineStyle {
-  rules: RuleSet
+export default (styleSheet) => {
+  class InlineStyle {
+    rules: RuleSet;
 
-  constructor(rules: RuleSet) {
-    this.rules = rules
-  }
-
-  generateStyleObject(executionContext: Object) {
-    const flatCSS = flatten(this.rules, executionContext).join('')
-    const hash = hashStr(flatCSS)
-    if (!generated[hash]) {
-      const root = parse(flatCSS)
-      const declPairs = []
-      const mediaObject = {}
-      root.each(node => {
-        if (node.type === 'decl') {
-          declPairs.push([node.prop, node.value])
-        } else if (node.type === 'atrule') {
-          mediaObject[`@${node.name} ${node.params}`] = transformDeclPairs(node.nodes.map((innerNode) =>
-            [innerNode.prop, innerNode.value]), excludeShorthands)
-        } else {
-          /* eslint-disable no-console */
-          console.warn(`Node of type ${node.type} not supported as an inline style`)
-        }
-      })
-      // RN currently does not support differing values for the corner radii of Image
-      // components (but does for View). It is almost impossible to tell whether we'll have
-      // support, so we'll just disable multiple values here.
-      // https://github.com/styled-components/css-to-react-native/issues/11
-      const styleObject = transformDeclPairs(declPairs, excludeShorthands)
-      const styles = StyleSheet.create({
-        generated: {
-          ...styleObject,
-          ...mediaObject,
-        },
-      })
-      generated[hash] = styles.generated
+    constructor(rules: RuleSet) {
+      this.rules = rules;
     }
-    return generated[hash]
+
+    generateStyleObject(executionContext: Object) {
+      const flatCSS = flatten(this.rules, executionContext).join('');
+
+      const hash = generateComponentId(flatCSS);
+      if (!generated[hash]) {
+        const root = parse(flatCSS);
+        const declPairs = [];
+        const mediaObject = {}
+        root.each(node => {
+          if (node.type === 'decl') {
+            declPairs.push([node.prop, node.value]);
+          } else if (node.type === 'atrule') {
+            mediaObject[`@${node.name} ${node.params}`] = transformDeclPairs(node.nodes.map((innerNode) =>
+              [innerNode.prop, innerNode.value]), excludeShorthands)
+          } else if (process.env.NODE_ENV !== 'production' && node.type !== 'comment') {
+            /* eslint-disable no-console */
+            console.warn(`Node of type ${node.type} not supported as an inline style`);
+          }
+        });
+        // RN currently does not support differing values for the corner radii of Image
+        // components (but does for View). It is almost impossible to tell whether we'll have
+        // support, so we'll just disable multiple values here.
+        // https://github.com/styled-components/css-to-react-native/issues/11
+        const styleObject = transformDeclPairs(declPairs, excludeShorthands)
+        const styles = StyleSheet.create({
+          generated: {
+            ...styleObject,
+            ...mediaObject,
+          },
+        });
+        generated[hash] = styles.generated;
+      }
+      return generated[hash];
+    }
   }
-}
+
+  return InlineStyle;
+};
